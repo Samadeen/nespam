9615af6d5842e01a298bd44046eff96d658ab30f
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-03-25 18:19:55 +0100

    Create format-date.ts

---
+++ utils/format-date.ts
@@ -0,0 +1,16 @@
+export const formatDate = (date: Date) => {
+  const day = date.getDate();
+  const ordinal = (n: number) => {
+    const s = ['th', 'st', 'nd', 'rd'];
+    const v = n % 100;
+    return n + (s[(v - 20) % 10] || s[v] || s[0]);
+  };
+
+  return date
+    .toLocaleDateString('en-US', {
+      weekday: 'long',
+      month: 'long',
+      year: 'numeric',
+    })
+    .replace(/\d+/, ordinal(day));
+};
