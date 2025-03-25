c6a4fa60d70938b18d9f51645b1c785a7ebcd6a3
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-03-25 18:14:39 +0100

    Create layout.tsx

---
+++ app/(user)/layout.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Header from '@/components/header';
+import Sidebar from '@/components/sidebar/page';
+import React, { ReactNode, Suspense } from 'react';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <Suspense fallback={'Loading...'}>
+      <div className='lg:relative'>
+        <div className='fixed z-[500] w-[18rem]'>
+          <Sidebar />
+        </div>
+        <div className={`ml-[18rem]`}>
+          <Header />
+          <div className='bg-[#fff] w-full py-[1.15rem] min-h-screen h-auto'>
+            {children}
+          </div>
+        </div>
+      </div>
+    </Suspense>
+  );
+};
+
+export default Layout;
