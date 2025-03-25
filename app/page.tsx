bddd8bc7c083e5a7792b9cfeb62b5bdcbc1a2102
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-03-25 18:14:52 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Image from 'next/image';
+import Login from './(auth)/login/page';
 
 export default function Home() {
-  return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-4xl font-bold'>Hello World</h1>
-    </div>
-  );
+  return <Login />;
 }
