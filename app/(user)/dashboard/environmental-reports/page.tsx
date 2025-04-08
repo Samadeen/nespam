7434c48a822bb2add6e3e9828f8a946ef5bafd4c
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-04-08 10:30:36 +0100

    Update page.tsx

--- app/(user)/dashboard/environmental-reports/page.tsx
+++ app/(user)/dashboard/environmental-reports/page.tsx
@@ -1,5 +1,11 @@
+import EnvironmentUpload from '@/components/consultant/consultant-environment/environment-upload';
+
 const EnvironmentalReports = () => {
-  return <div>EnvironmentalReports</div>;
+  return (
+    <div>
+      <EnvironmentUpload />
+    </div>
+  );
 };
 
 export default EnvironmentalReports;
