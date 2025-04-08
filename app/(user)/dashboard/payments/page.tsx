1a0ae77f11614c878003dc26dd8bb7020063c20f
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-04-08 10:30:28 +0100

    Update page.tsx

--- app/(user)/dashboard/payments/page.tsx
+++ app/(user)/dashboard/payments/page.tsx
@@ -1,5 +1,13 @@
+'use client';
+
+import ConsultantPaymentTable from '@/components/consultant/consultant-payment/payment-table';
+
 const Payments = () => {
-  return <div>Payments</div>;
+  return (
+    <div className='px-9 py-6'>
+      <ConsultantPaymentTable />
+    </div>
+  );
 };
 
 export default Payments;
