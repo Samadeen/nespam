bf51ce2bbd1ba62cb98505cfa5efd646daa54dee
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-04-08 10:30:22 +0100

    Update page.tsx

--- app/(user)/dashboard/support/page.tsx
+++ app/(user)/dashboard/support/page.tsx
@@ -1,5 +1,13 @@
+import SupportTable from '@/components/consultant/consultant-support/support-table';
+import SupportSystem from '@/components/consultant/consultant-support/support-system';
+
 const Support = () => {
-  return <div>Support</div>;
+  return (
+    <div className='px-9 py-6'>
+      {/* <SupportTable /> */}
+      <SupportSystem />
+    </div>
+  );
 };
 
 export default Support;
