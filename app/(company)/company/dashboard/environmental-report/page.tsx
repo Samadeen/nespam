dcda320dd15d689c343cba094a614ff596053449
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-04-08 10:26:05 +0100

    Create page.tsx

---
+++ app/(company)/company/dashboard/environmental-report/page.tsx
@@ -0,0 +1,11 @@
+import EnvironmentUpload from '@/components/consultant/consultant-environment/environment-upload';
+
+const CompanyEnvironmentalReports = () => {
+  return (
+    <div>
+      <EnvironmentUpload />
+    </div>
+  );
+};
+
+export default CompanyEnvironmentalReports;
