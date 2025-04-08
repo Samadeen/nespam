b12216af5d75b6edca4ddc8ee859f99813222bd4
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-04-08 10:30:40 +0100

    Update page.tsx

--- app/(user)/dashboard/documents/page.tsx
+++ app/(user)/dashboard/documents/page.tsx
@@ -1,5 +1,11 @@
+import DocumentTable from '@/components/consultant/consultant-document/document-table';
+
 const Documents = () => {
-  return <div>Documents</div>;
+  return (
+    <div className='px-9 py-6'>
+      <DocumentTable />
+    </div>
+  );
 };
 
 export default Documents;
