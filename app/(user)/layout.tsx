0a522674aa268b31fb9b208873da3815b06c3749
Momoh Abdulsamad <samadmomoh@gmail.com>
2025-04-08 10:30:17 +0100

    Update layout.tsx

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -12,10 +12,10 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <Suspense fallback={'Loading...'}>
       <div className='lg:relative'>
-        <div className='fixed z-[500] w-[18rem]'>
+        <div className='fixed z-[500] w-[19rem]'>
           <Sidebar />
         </div>
-        <div className={`ml-[18rem]`}>
+        <div className={`ml-[19rem]`}>
           <Header />
           <div className='bg-[#fff] w-full py-[1.15rem] min-h-screen h-auto'>
             {children}
